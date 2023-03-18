538d49e39820870ecad7f8561a439ecdb2272cdc
Otavio Souza <otaviosouzalu@gmail.com>
2023-03-18 17:06:25 -0300

    add Api's

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,15 @@
+
+export default function initFetachBitcoin() {
+
+
+}
+
+fetch('https://blockchain.info/ticker')
+.then(response=>response.json())
+.then(json=>{
+  const btcPreco = document.querySelector(".btn-preco")
+btcPreco.innerText = (1000 / json.BRL.sell).toFixed(4)
+})
+.catch(erro=>{
+  console.log(Error(erro))
+})
--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropMenu from './modules/drop-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetachAnimais from './modules/fetch-animais.js';
+import initFetachBitcoin from './modules/fetch-bitcoin.js';
 
 initScrollSuave();
 animacaoScroll();
@@ -17,5 +18,6 @@ initModal();
 initTooltip();
 initDropMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetachAnimais();
+initFetachBitcoin();
