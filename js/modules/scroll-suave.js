bd24279ffb0c395e6bd86f9d38b7b08e8184cfb4
Otavio Souza <otaviosouzalu@gmail.com>
2023-04-07 01:05:34 -0300

    Scroll Suave refatorado e transformado em objeto

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,19 +1,31 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll(
-    '[data-menu="suave"] a[href^="#"]',
-  );
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
+    this.scrolltoSection = this.scrolltoSection.bind(this);
+  }
 
-  function scrolltoSection(event) {
+  scrolltoSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
 
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrolltoSection);
     });
   }
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrolltoSection);
-  });
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+      return this;
+    }
+  }
 }
