d291cb4a78f5f744829dff06bc4b6e8d8fd82f49
Otavio Souza <otaviosouzalu@gmail.com>
2023-04-07 22:56:52 -0300

    Animacao ao sroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,29 +1,47 @@
 export default class ScrollAnima {
-  constructor(sections){
-   this.sections = document.querySelectorAll(sections);
-  this.windowMetade = window.innerHeight * 0.6;
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
 
-  this.animaScroll = this.animaScroll.bind(this)
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
+  //verifica a distancia de cada item em relacao ao ao topo 
+  //do site e ocloca em um objeto
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
 
-
- animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top - this.windowMetade;
-
-      if (sectionTop < 0) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+  //verifica a distancia entre o top do elemento
+  // e o scroll e add class ativo
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
-init(){
-  this.animaScroll()
-  window.addEventListener('scroll', this.animaScroll);
-}
+  //inicia ao carregar a animacao
+  init() {
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this
+  }
 
-
+  //remove o event de scroll
+  stop(){
+    window.removeEventListener('scroll', this.checkDistance);
+  }
 }
